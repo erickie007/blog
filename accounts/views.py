09e1f4750568ffc006faa6f8bde11b8745f445a2
ecosyst <admin@ecosyst.net>
2020-03-21 16:13:50 +0300

    Project Platform

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+def signup_view(request):
+    if request.method = 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form':form})
